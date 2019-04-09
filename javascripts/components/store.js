597f3524ce52ebd5777af27aa4758166b38ce1f1
Bob <bob.e.baxter@gmal.com>
2019-04-08 21:33:48 -0500

    wip

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,7 +1,12 @@
+import bookList from '../helpers/book.js';
 import util from '../helpers/util.js';
 
 const makeStore = () => {
-  let domString = `<h3>Help</h3>`;
+  const books = bookList.getBook();
+  let domString = `<h2>Our Only Book</h2>`;
+  domString += `<h3>${books.price}</h3>`;
+  domString += `<img src=${books.image} alt="book cover"</h3>`;
+  domString += `<button class="btn btn-danger">Add to Cart</button>`;
   util.printToDom('store-container', domString);
 };
 
